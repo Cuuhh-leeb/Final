008a0ac1379b1adf8eb6c49e1f4cf8186883c0a8
Calieb Simpson <caliebsimpson@caliebs-mbp-2.attlocal.net>
2020-06-19 14:58:09 -0400

    FinalFinalDraft

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
 public class Dictionary {
-    public static void main(String []args) {
-        int num, i, j;
+    public static void main(String[] args) {
+        int num;
+        int i;
+        int j;
+        String temp;
+
+
         Scanner input = new Scanner(System.in);
 
 
@@ -13,25 +18,45 @@ public class Dictionary {
 
 
         System.out.println("Enter " + num + " words: ");
-        String temp;
-        for (i = 0; i < num; i++)
-            array[i] = input.nextLine();
+        Scanner words = new Scanner(System.in);
+        String str;
+        // str = words.nextLine();
+        System.out.println("Entered words");
 
-        for (i = 0; i < ( num - 1 ); i++) {
+        for (i = 0; i < num; i++) {
+            array[i] = input.next();
+        }
+        System.out.println("Alphabetical order");
+
+
+        for (i = 0; i < (num - 1); i++) {
             for (j = 0; j < num - i - 1; j++) {
-                if (array[j].compareToIgnoreCase(array[j+1]) < 0)
-                {
+                if (array[j].compareToIgnoreCase(array[j + 1]) > 0) {
                     temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
+                for (i = 0; i < num; i++)
+                    System.out.println(array[i]);
+
+
+                System.out.println("Reverse order");
+
+                for (i = 0; i < (num - 1); i++) {
+                    for (j = 0; j < num - i - 1; j++) {
+                        if (array[j].compareToIgnoreCase(array[j + 1]) < 0) {
+                            temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
+                        }
+                    }
+                }
+
+                for (i = 0; i < num; i++)
+                    System.out.println(array[i]);
             }
         }
-
-        System.out.println("Sorted list of words:");
-
-        for (i = 0; i < num; i++)
-            System.out.println(array[i]);
     }
 }
 
